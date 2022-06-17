a3f87fb477d11725a2ccee993456c70ffa3c1e68
Jake Tellep <jj.tellep@gmail.com>
2022-06-17 17:13:18 -0400

    added basic styling to styled component

--- src/components/Card.js
+++ src/components/Card.js
@@ -12,6 +12,17 @@ const Card = () => {
   );
 };
 
-const StyledCard = styled.article``;
+const StyledCard = styled.article`
+  width: 90vw;
+  max-width: 300px;
+  background: var(--white);
+  border-radius: 0.25rem;
+
+  img {
+    width: 100%;
+    border-top-left-radius: 0.25rem;
+    border-top-right-radius: 0.25rem;
+  }
+`;
 
 export default Card;

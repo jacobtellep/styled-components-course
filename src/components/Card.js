37384cf3a588aea244bd67f53547a2df9ad8968d
Jake Tellep <jj.tellep@gmail.com>
2022-06-17 17:28:51 -0400

    showing how to use pseudo elements and hovers and transition animations in a styled component

--- src/components/Card.js
+++ src/components/Card.js
@@ -28,14 +28,30 @@ const StyledCard = styled.article`
     justify-content: space-between;
     align-items: center;
     padding: 0 2rem;
+
     h4 {
       text-transform: capitalize;
+      &::before {
+        content: 'Name : ';
+        color: red;
+      }
     }
 
     p {
       color: var(--primary);
       font-weight: 700;
     }
+  }
+  // it seems that the transion is working on the hover below it?
+  // I guess that in order to have transitions they need to be right before the action to trigger the transition?
+  transition: all 0.25s ease-in-out;
+  &:hover {
+    box-shadow: 0 3px 3px #222;
+    cursor: pointer;
+  }
+
+  @media (min-width: 768px) {
+    max-width: 600px;
   }
 `;
 

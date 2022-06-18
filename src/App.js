2ea2743fd9817fc2cac32d7511188bb083898acb
Jake Tellep <jj.tellep@gmail.com>
2022-06-17 22:39:23 -0400

    basic theming

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
+import styled, { ThemeProvider } from 'styled-components';
 import { StyledEngineProvider } from '@mui/material/styles'; // needed to override mui styles
-import Card from './components/Card';
 import GlobalStyles from './global-styles';
+import { useState } from 'react';
+
+const BaseTheme = {
+  color: '#222',
+  background: '#fff',
+};
+
+const DarkTheme = {
+  color: '#fff',
+  background: '#222',
+};
+
+const Container = styled.div`
+  padding: 2rem;
+  color: ${(props) => props.theme.color};
+  background: ${(props) => props.theme.background};
+`;
 
 function App() {
+  const [baseTheme, setTheme] = useState(true);
+
+  const toggleTheme = () => {
+    setTheme(!baseTheme);
+  };
   return (
     <StyledEngineProvider injectFirst>
-      <div style={{ padding: '2rem' }}>
+      <ThemeProvider theme={baseTheme ? BaseTheme : DarkTheme}>
         <GlobalStyles />
-        <Card />
-      </div>
+        <Container>
+          <h1>hello world</h1>
+          <button className='btn' onClick={toggleTheme}>
+            toggle me
+          </button>
+        </Container>
+      </ThemeProvider>
     </StyledEngineProvider>
   );
 }
